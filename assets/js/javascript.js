0f2ec1860f5e94c36e2c8df554335dcce4487d76
AngryMoustache <mrclotion@hotmail.com>
2016-07-26 10:50:13 +0200

    Added google calender support

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -18,28 +18,48 @@ function doSomething()
     
 };*/
 
-(function() {
+function exportTableToCSV($table, filename) {
 
-})();
+    var $rows = $table.find('tr:has(td)'),
 
-var menudown2 = document.querySelector("#mailme");
-var action2 = document.querySelector("#mail");
-var flick2 = 1;
+        // Temporary delimiter characters unlikely to be typed by keyboard
+        // This is to avoid accidentally splitting the actual contents
+        tmpColDelim = String.fromCharCode(11), // vertical tab character
+        tmpRowDelim = String.fromCharCode(0), // null character
 
-action2.addEventListener("click",doSomething2);
+        // actual delimiter characters for CSV format
+        colDelim = '","',
+        rowDelim = '"\r\n"',
 
-function doSomething2()
-{
-    if(flick2 == 1)
-    {
-        menudown2.style.bottom = '150px'; 
-    flick2 = 0;
-    }
-    else{
-         menudown2.style.bottom = '205px'; 
-    flick2 = 1;
-    }
-};
+        // Grab text from table into CSV formatted string
+        csv = '"' + $rows.map(function (i, row) {
+            var $row = $(row),
+                $cols = $row.find('td');
+
+            return $cols.map(function (j, col) {
+                var $col = $(col),
+                    text = $col.text();
+
+                return text.replace(/"/g, '""'); // escape double quotes
+
+            }).get().join(tmpColDelim);
+
+        }).get().join(tmpRowDelim)
+            .split(tmpRowDelim).join(rowDelim)
+            .split(tmpColDelim).join(colDelim) + '"',
+
+        // Data URI
+        csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+
+    $(this)
+        .attr({
+        'download': filename,
+            'href': csvData,
+            'target': '_blank'
+    });
+    
+    location.reload();
+}
 
 // Message window
 function showMessage(title, message)
@@ -69,6 +89,33 @@ var email = document.querySelector('input[name=email]');
 // This is temporary
 var calender = "";
 
+// Click events
+var buttons = $(".button-n-field");
+buttons.on("click", buttonFlick);
+function buttonFlick()
+{
+    if ($(this).hasClass('google'))
+    {
+        if (calender == '')
+        {
+            calender = 'google';
+        }
+        else
+        {
+            calender = '';
+        }
+        console.log(calender);
+    }
+    if($(this).children('.notify-field').css('bottom') == '205px')
+    {
+        $(this).children('.notify-field').css('bottom', '150px'); 
+    }
+    else
+    {
+        $(this).children('.notify-field').css('bottom', '205px');  
+    }
+};
+
 function sendData()
 {
     // Check everything
@@ -84,7 +131,7 @@ function sendData()
 
         $.ajax({
             "crossDomain": true,
-            "url": "http://localhost:8000/add",
+            "url": "http://localhost:80/add",
             "method": "POST",
             "headers": {
                 "content-type": "application/x-www-form-urlencoded"
@@ -95,6 +142,12 @@ function sendData()
             console.log(response);
         });
     }
+
+}
+
+function formatDate(d){
+    function pad(n){return n<10 ? '0'+n : n}
+    return d.getUTCFullYear() + '/' + pad(d.getMonth()+1) + '/' + pad(d.getDate())
 }
 
 function checkForm()
@@ -147,6 +200,58 @@ function checkForm()
 
             if (inputDate.setHours(0,0,0,0) > todaysDate.setHours(0,0,0,0))
             {
+                // Setup data for calender
+                if (calender == "google")
+                {
+                    $(".Start_Date_Later").html(date.value);
+
+                    if (parseInt(time_end_hour.value) < parseInt(time_start_hour.value))
+                    {
+                        // Event lasts untill next day
+                        var newdate = new Date(date.value);
+                        newdate.setDate(newdate.getDate() + 1);
+                        $(".End_Date_Later").html(formatDate(newdate));
+                        console.log(formatDate(newdate));
+                    }
+                    else
+                    {
+                        $(".End_Date_Later").html(date.value);
+                    }
+
+                    $(".Start_Time").html(time_start_hour.value + ":" + time_start_minutes.value);
+                    $(".End_Time").html(time_end_hour.value + ":" + time_end_minutes.value);
+
+                    // Reduce a week
+                    var today = new Date(date.value); 
+                    var lastWeekDate = new Date(today.setDate(today.getDate() - 7));
+                    var formattedDate = formatDate(lastWeekDate);
+
+                    $(".Start_Date").html(formattedDate);
+
+                    if (parseInt(time_end_hour.value) < parseInt(time_start_hour.value))
+                    {
+                        // Event lasts untill next day
+                        var newdate = new Date(formattedDate);
+                        newdate.setDate(newdate.getDate() + 1);
+                        $(".End_Date").html(formatDate(newdate));
+                        console.log(formatDate(newdate));
+                    }
+                    else
+                    {
+                        $(".End_Date").html(formattedDate);
+                    }
+
+                    // Click event export
+                    $("#ok").parent().addClass("export");
+                    $("#ok").parent().attr("href", "#");
+                    $("#ok").text("Downloaden");
+
+                    $(".export").on('click', function (event) {
+                        $("#ok").text("OK");
+                        exportTableToCSV.apply(this, [$('#dvData_'+calender+'>table'), 'export.csv']);
+                    });
+                }
+
                 // SEND THE DATA
                 console.log('%cSUCCESS: everything is fine!', 'background: green; color: white;');
                 return true;
@@ -168,7 +273,7 @@ function checkForm()
     }
     else
     {
-        showMessage('Oops!', 'Noteer een telefoonnummer, email of selecteer een kalendermethode!');
+        showMessage('Oops!', 'Noteer een telefoonnummer, email of selecteer de kalender!');
         console.log('%cERROR: insert either calender, phone or email!', 'color: red');
         return false;
     }
